0c1579f23c668615c1ac4aa57bf50a656616a361
edohodz <hodzicedo64@gmail.com>
2026-06-11 23:51:10 +0200

    Documentazione package cinemax

---
+++ src/cinemax/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ * Il seguente package rappresenta la radice dell'applicazione
+ * che ne permette la sua esecuzione.
+ * </p>
+ * <p>
+ * Possiede dei campi final {@code MARGIN}, {@code MARKER}, {@code ROW_LENGTH}
+ * per il controllo del formato di output dell'intera applicazione.
+ * </p>
+ * @see cinemax.CineMax#main(java.lang.String[])
+ */
+package cinemax;
